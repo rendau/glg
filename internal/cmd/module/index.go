a8d65aa813a6d044d9a628b9d8bf8c9e820a7553
Dauren <dauren.asir@gmail.com>
2021-05-09 19:01:42 +0600

    some changes

--- internal/cmd/module/index.go
+++ internal/cmd/module/index.go
@@ -4,6 +4,10 @@ import (
 	"log"
 
 	"github.com/rendau/glg/internal/cmd/core"
+	"github.com/rendau/glg/internal/cmd/db"
+	"github.com/rendau/glg/internal/cmd/interfaces"
+	"github.com/rendau/glg/internal/cmd/rest"
+	"github.com/rendau/glg/internal/cmd/usecases"
 	"github.com/rendau/glg/internal/entity"
 	"github.com/rendau/glg/internal/project"
 )
@@ -23,9 +27,9 @@ func Run(dir, name string) {
 
 	// fmt.Println(ent)
 
-	// db.Make(pr, eName, ent)
-	// interfaces.Make(pr, eName, ent)
+	db.Make(pr, eName, ent)
+	interfaces.Make(pr, eName, ent)
 	core.Make(pr, eName, ent)
-	// usecases.Make(pr, eName, ent)
-	// rest.Make(pr, eName, ent)
+	usecases.Make(pr, eName, ent)
+	rest.Make(pr, eName, ent)
 }
